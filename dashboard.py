9d219f1f854b492b636681e5ab1e3bf979d348ee
Anonim19992 <acgyf11@mail.ru>
2023-11-15 15:38:22 +1000

    test

---
+++ dashboard.py
@@ -0,0 +1,67 @@
+from dash import Dash, html, dcc
+import plotly.express as px
+import plotly.graph_objs as go
+import pandas as pd
+
+app = Dash(__name__)
+
+# Таблица
+df4 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+# Линейный график
+df1 = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+fig1 = px.line(df1, x="Fruit", y="Amount", color="City")
+# Гистограмма
+df3 = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2]
+})
+fig3 = px.histogram(df3, x="Fruit", y="Amount", barmode="group")
+# График рассеяния
+df5 = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 3, 4],
+    "size": [2, 1, 2, 2, 2, 1],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+fig5 = px.scatter(df5, x="Fruit", y="Amount", color="City", size="size")
+# Круговая диаграмма
+df6 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+fig6 = px.pie(df6, values='total exports', names='state')
+
+app.layout = html.Div(children=[
+    generate_table(df4),
+    dcc.Graph(
+        id='example-graph-6',
+        figure=fig6
+    ),
+    dcc.Graph(
+        id='example-graph-5',
+        figure=fig5
+    ),
+    dcc.Graph(
+        id='example-graph-3',
+        figure=fig3
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig1
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
